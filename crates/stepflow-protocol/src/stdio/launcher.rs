7445ec887afe7528a577e252234de60c162fb94e
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2025-05-28 12:31:11 -0700

    simplify environment rules for now

--- crates/stepflow-protocol/src/stdio/launcher.rs
+++ crates/stepflow-protocol/src/stdio/launcher.rs
@@ -8,33 +8,11 @@ use tokio::process::Child;
 
 use super::StdioError;
 
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(untagged)]
-pub enum InheritEnv {
-    /// List of environment variables to inherit.
-    Whitelist(Vec<String>),
-    /// Inherit all environment variables or none.
-    AllOrNone(bool),
-}
-
-impl Default for InheritEnv {
-    fn default() -> Self {
-        Self::AllOrNone(false)
-    }
-}
-
-impl InheritEnv {
-    pub fn is_default(&self) -> bool {
-        matches!(self, Self::AllOrNone(true))
-    }
-}
-
 /// Helper for launching a sub-process.
 pub(crate) struct Launcher {
     working_directory: PathBuf,
     pub command: PathBuf,
     pub args: Vec<OsString>,
-    inherit_env: InheritEnv,
     env: IndexMap<String, String>,
 }
 
@@ -43,7 +21,6 @@ impl Launcher {
         working_directory: PathBuf,
         command: String,
         args: Vec<String>,
-        inherit_env: InheritEnv,
         env: IndexMap<String, String>,
     ) -> Result<Self> {
         let command = which::WhichConfig::new()
@@ -58,7 +35,6 @@ impl Launcher {
             working_directory,
             command,
             args: args.into_iter().map(|s| s.into()).collect(),
-            inherit_env,
             env,
         })
     }
@@ -72,26 +48,10 @@ impl Launcher {
             .stdout(Stdio::piped())
             .kill_on_drop(true);
 
-        // First setup the inheritted environment.
-        match &self.inherit_env {
-            InheritEnv::AllOrNone(true) => {
-                // Nothing to filter.
-            }
-            InheritEnv::AllOrNone(false) => {
-                command.env_clear();
-            }
-            InheritEnv::Whitelist(whitelist) => {
-                command.env_clear();
-                for key in whitelist {
-                    if let Some(value) = std::env::var_os(key) {
-                        command.env(key, value);
-                    }
-                }
-            }
-        }
-
-        // Then setup oter environment variables.
+        // Only pass explicit environment variables through.
+        command.env_clear();
         for (key, value) in self.env.iter() {
+            // TODO: Allow value to be a template referencing parent environment variables.
             command.env(key, value);
         }
 
