2d5aec7a86f29e54df1b7c6e4526b94138b20bfe
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2025-07-21 10:12:13 -0700

    docs: initial draft of announcement blog

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -81,7 +81,7 @@ const config: Config = {
           position: 'left',
           label: 'Docs',
         },
-        // { to: '/blog', label: 'Blog', position: 'left' },
+        { to: '/blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/riptano/stepflow',
           label: 'GitHub',
@@ -121,10 +121,10 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            // {
-            //   label: 'Blog',
-            //   to: '/blog',
-            // },
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
             {
               label: 'GitHub',
               href: 'https://github.com/riptano/stepflow',
