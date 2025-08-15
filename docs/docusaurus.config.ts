1b565204d9e4bc1daecf35d80d18db3f12a75bfa
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2025-08-14 17:07:34 -0700

    docs: footer copyright (#215)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -132,7 +132,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Stepflow Contributors. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
