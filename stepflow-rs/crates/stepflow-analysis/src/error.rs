9ecd5830d343800a26411313e008b5d996afcb50
Ben Chambers <ben.chambers@datastax.com>
2025-12-09 13:29:05 -0800

    chore: lint

--- stepflow-rs/crates/stepflow-analysis/src/error.rs
+++ stepflow-rs/crates/stepflow-analysis/src/error.rs
@@ -10,8 +10,6 @@
 // or implied. See the License for the specific language governing permissions and limitations under
 // the License.
 
-use crate::Diagnostics;
-
 /// Error types for workflow analysis (internal/unexpected errors only)
 #[derive(Debug, thiserror::Error)]
 pub enum AnalysisError {
